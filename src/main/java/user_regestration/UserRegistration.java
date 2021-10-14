6cc039010548121205cef17634ad91940a98f611
surjya <surjyamaniduan@gmail.com>
2021-10-14 12:15:20 +0530

    Add:validate Last Name

--- src/main/java/user_regestration/UserRegistration.java
+++ src/main/java/user_regestration/UserRegistration.java
@@ -1,5 +1,46 @@
 package user_regestration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+
+	static Scanner scanner= new Scanner(System.in);
+	public static void validFirstName() {
+
+		System.out.println("Enter First Name:");
+		String name=scanner.next();
+		String regex="^[A-Z]{1}[a-z]*";
+
+		Pattern p=Pattern.compile(regex);
+		Matcher m=p.matcher(name);
+		boolean r=m.matches();
+		
+		if (r)
+            System.out.println("First name is valid");
+        else
+            System.out.println("First name is !Invalid");
+		
+		
+	}
+	public static void validLastName() {
+
+		System.out.println("Enter Last Name:");
+		String lname=scanner.next();
+		String regex="^[A-Z]{1}[a-z]{2,}$";
+
+		Pattern p=Pattern.compile(regex);
+		Matcher m=p.matcher(lname);
+		boolean r=m.matches();
+		
+		if (r)
+            System.out.println("Last name is valid");
+        else
+            System.out.println("Last name is !Invalid");
+		
+		
+	}
+
 
 }
--- src/main/java/user_regestration/UserRegistrationMain.java
+++ src/main/java/user_regestration/UserRegistrationMain.java
@@ -1,5 +1,42 @@
 package user_regestration;
 
-public class UserRegistrationMain {
+import java.util.Scanner;
 
+public class UserRegistrationMain {
+	public static void welcome() {
+		System.out.println("Welcome to user registation");
+	}
+
+	public static void main(String[] args) {
+		welcome();
+		UserRegistration user=new UserRegistration();
+		while(true) {
+
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter 0 : Exit");
+			System.out.println("Enter 1 : to validate First Name ");
+			System.out.println("Enter 2 : to validate Last Name ");
+			
+			switch (scanner.nextInt()){
+			case 0:			
+				System.exit(0);
+			
+			case 1:
+				UserRegistration.validFirstName();
+				break;
+
+
+			case 2:
+				UserRegistration.validLastName();
+				break;
+			
+					
+			
+			
+			
+			
+			}
+
+		}
+	}
 }
